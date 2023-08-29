6a28ed8915f46a0f8be7ecc08dcd60988e512fdc
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2023-08-29 10:41:22 -0300

    Atualizando

--- funcionario.ts
+++ funcionario.ts
@@ -2,6 +2,8 @@ class Funcionario{
     nome: string;
     cargo: string;
     salario: number;
+    inss: number = 11
+    IR: number = 7.5
 
 
     constructor(nome: string, cargo: string, salario: number)
@@ -9,18 +11,38 @@ class Funcionario{
         this.nome = nome;
         this.cargo = cargo;
         this.salario = salario;
+       
 
     }
-    INSS(desconto: number){
-        let valorDesconto = ((this.salario/100) * desconto);
-        return "Foi reduzido de seu salario " + desconto + " % do INSS. Voce ira receber " + (this.salario - valorDesconto);
+    imposto(){
+        let valorDescontoInss =  ((this.salario/100)* this.inss);
+        let salarioComDescontoInss = this.salario - valorDescontoInss;
+        let valorImpostoDeRenda = ((salarioComDescontoInss/100)* this.IR);
+        return valorDescontoInss + valorImpostoDeRenda  
+       
+       
+    }
+    aumento(aumentos: number){
+        return "Aumento de "+ aumentos +"% aplicado. Novo salário:"+(((this.salario/100)*aumentos) + this.salario);
+    }
+    detalheDoFuncionario(){
+        let imposto = this.imposto();
+        return "Funcionario: " + this.nome + ", cargo: "+this.cargo+ ", salario bruto R$"+this.salario+ ", impostos: "+ imposto+ ", salrio liquido R$"+(this.salario - imposto);
     }
 
-
+ 
 
 
 }
 let funcionario = new Funcionario("Ana Silva", "Analista de Vendas", 3000);
+let funcionario2 = new Funcionario("Luffy", "Advogado", 50000);
 
 console.log(funcionario);
-console.log(funcionario.INSS)
+console.log(funcionario.imposto());
+console.log(funcionario.aumento(10));
+console.log(funcionario.detalheDoFuncionario());
+
+console.log(funcionario2);
+console.log(funcionario2.imposto());
+console.log(funcionario2.aumento(10));
+console.log(funcionario2.detalheDoFuncionario());
