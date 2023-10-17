d385c8386db8de17af73e6284a7c41e2c45a3858
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2023-10-17 08:01:09 -0300

    Atualizado

---
+++ pedido.js
@@ -0,0 +1,8 @@
+"use strict";
+var StatusPedido;
+(function (StatusPedido) {
+    StatusPedido["AguardandoPreparo"] = "Aguardando preparo";
+    StatusPedido["EmPreparo"] = "Em preparo";
+    StatusPedido["SaiuParaEntrega"] = "Saiu para entrega";
+    StatusPedido["Entregue"] = " Entregue";
+})(StatusPedido || (StatusPedido = {}));
