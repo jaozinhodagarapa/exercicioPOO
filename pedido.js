097483f3e7ac45b55f358588c171660de35c22d0
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2023-10-17 13:22:50 -0300

    Atualizado

--- pedido.js
+++ pedido.js
@@ -6,3 +6,40 @@ var StatusPedido;
     StatusPedido["SaiuParaEntrega"] = "Saiu para entrega";
     StatusPedido["Entregue"] = " Entregue";
 })(StatusPedido || (StatusPedido = {}));
+class ProdutoPedido {
+    constructor(nome, valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+}
+class Pedido {
+    constructor() {
+        this.produtos = [];
+        this.status = StatusPedido.AguardandoPreparo;
+    }
+    adicionarProduto(produto) {
+        this.produtos.push(produto);
+        return "Produto adicionado com sucesso!!!";
+    }
+    atualizarStatus(status) {
+        this.status = status;
+    }
+    exibirStaus() {
+        return this.status;
+    }
+    exibirProdutosDoPedido() {
+        console.log("Produtos do pedidos: ");
+        for (let index = 0; index < this.produtos.length; index++) {
+            console.log("Produto :" + this.produtos[index].nome + " R$ " + this.produtos[index].valor.toFixed(2));
+        }
+    }
+}
+const produtoPedido = new ProdutoPedido("Pizza", 30);
+const outroProdutoPedido = new ProdutoPedido("X-Frango", 15);
+const novoPedido = new Pedido();
+novoPedido.adicionarProduto(produtoPedido);
+novoPedido.adicionarProduto(outroProdutoPedido);
+console.log("Status atual do pedido " + novoPedido.exibirStaus());
+novoPedido.atualizarStatus(StatusPedido.EmPreparo);
+console.log("Status atual do pedido " + novoPedido.exibirStaus());
+novoPedido.exibirProdutosDoPedido();
--- pedido.ts
+++ pedido.ts
@@ -5,3 +5,49 @@ enum StatusPedido{
     Entregue = " Entregue"
 }
 
+class ProdutoPedido{
+    nome:string;
+    valor:number;
+
+    constructor(nome: string, valor: number){
+        this.nome = nome;
+        this.valor = valor;
+    }
+}
+
+class Pedido{
+    
+    private produtos:ProdutoPedido[];
+    private status:StatusPedido;
+
+    constructor(){
+        this.produtos = [];
+        this.status = StatusPedido.AguardandoPreparo;
+    }
+    adicionarProduto(produto:ProdutoPedido){
+        this.produtos.push(produto);
+        return "Produto adicionado com sucesso!!!"
+    }
+    atualizarStatus(status: StatusPedido){
+        this.status = status;
+    }
+    exibirStaus(){
+        return this.status
+    }
+    exibirProdutosDoPedido(){
+        console.log("Produtos do pedidos: ")
+        for (let index = 0; index < this.produtos.length; index++){
+            console.log("Produto :" + this.produtos[index].nome + " R$ " + this.produtos [index].valor.toFixed(2));
+        }
+    }
+}
+
+const produtoPedido = new ProdutoPedido("Pizza", 30);
+const outroProdutoPedido = new ProdutoPedido("X-Frango",15);
+const novoPedido = new Pedido();
+novoPedido.adicionarProduto(produtoPedido);
+novoPedido.adicionarProduto(outroProdutoPedido);
+console.log("Status atual do pedido " + novoPedido.exibirStaus());
+novoPedido.atualizarStatus(StatusPedido.EmPreparo);
+console.log("Status atual do pedido " + novoPedido.exibirStaus());
+novoPedido.exibirProdutosDoPedido();
